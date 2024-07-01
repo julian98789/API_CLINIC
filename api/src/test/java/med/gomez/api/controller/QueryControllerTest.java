95ca447df4fb42d95fdffebcb35e9111cdd76054
julian98789 <julian_gomez69222@elpoli.edu.co>
2024-06-30 19:16:09 -0500

    empaquete la api

--- api/src/test/java/med/gomez/api/controller/QueryControllerTest.java
+++ api/src/test/java/med/gomez/api/controller/QueryControllerTest.java
@@ -6,7 +6,6 @@ import med.gomez.api.domain.query.DataScheduleQuery;
 import med.gomez.api.domain.query.QueryAgendaService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,9 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.time.LocalDateTime;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -31,25 +28,25 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 class QueryControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc; //Se utiliza para simular solicitudes HTTP en los tests sin necesidad de un servidor real.
 
     @Autowired
-    private JacksonTester<DataScheduleQuery> dataScheduleQueryJacksonTester;
+    private JacksonTester<DataScheduleQuery> dataScheduleQueryJacksonTester; // Para serializar y deserializar JSON
 
     @Autowired
-    private JacksonTester<DataDetailQueries> dataDetailQueriesJacksonTester;
+    private JacksonTester<DataDetailQueries> dataDetailQueriesJacksonTester; // Para serializar y deserializar JSON
 
     @MockBean
-    private QueryAgendaService queryAgendaService;
+    private QueryAgendaService queryAgendaService; // Mock del servicio que maneja las consultas
 
-    @WithMockUser
+    @WithMockUser //Simula un usuario autenticado para los tests de seguridad.
     @Test
     @DisplayName("Deberia retornar un estado http 400 cuando los datos ingresados sean invalidos")
     void scheduleScenery1() throws Exception {
-        //dado / cuando
+        // Dado / cuando: se realiza una solicitud POST sin contenido
         var response = mockMvc.perform(post("/consultas")).andReturn().getResponse();
 
-        //entonces
+        // Entonces: se espera un estado HTTP 400 (Bad Request)
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
@@ -58,24 +55,25 @@ class QueryControllerTest {
     @DisplayName("Deberia retornar un estado http 200 cuando los datos ingresados sean validos")
     void scheduleScenery2() throws Exception {
 
-        //dado
-        var fecha = LocalDateTime.now().plusHours(1);
-        var specialty = Specialty.CARDIOLOGY;
-        var data = new DataDetailQueries(null,5l,5l,fecha);
+        // Dado: configuración de datos válidos y un mock del servicio
+        var fecha = LocalDateTime.now().plusHours(1); // Fecha en el futuro
+        var specialty = Specialty.CARDIOLOGY; // Especialidad
+        var data = new DataDetailQueries(null, 5L, 5L, fecha); // Datos de detalle de la consulta
 
-        // cuando
+        // Cuando: se simula la respuesta del servicio al agendar
         when(queryAgendaService.schedule(any())).thenReturn(data);
 
+        // Se realiza una solicitud POST con datos válidos
         var response = mockMvc.perform(post("/consultas")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(dataScheduleQueryJacksonTester.write(new DataScheduleQuery(2l,5l,fecha, specialty)).getJson()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dataScheduleQueryJacksonTester.write(new DataScheduleQuery(2L, 5L, fecha, specialty)).getJson()))
                 .andReturn().getResponse();
 
-        //entonces
+        // Entonces: se espera un estado HTTP 200 (OK)
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 
-        var expectedJeson = dataDetailQueriesJacksonTester.write(data).getJson();
-        assertThat(response.getContentAsString()).isEqualTo(expectedJeson);
-
+        // Se verifica que el contenido de la respuesta sea igual al JSON esperado
+        var expectedJson = dataDetailQueriesJacksonTester.write(data).getJson();
+        assertThat(response.getContentAsString()).isEqualTo(expectedJson);
     }
 }
--- api/src/test/java/med/gomez/api/domain/medic/MedicalRepositoryTest.java
+++ api/src/test/java/med/gomez/api/domain/medic/MedicalRepositoryTest.java
@@ -19,70 +19,72 @@ import java.time.temporal.TemporalAdjusters;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest //Se utiliza para probar la capa de persistencia con JPA.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) //Configura el uso de la base de datos para las pruebas y no remplasa la base de datos real
 @ActiveProfiles("test")
 class MedicalRepositoryTest {
 
     @Autowired
-    private MedicalRepository medicalRepository;
+    private MedicalRepository medicalRepository; // Repositorio de medicos
 
     @Autowired
-    private TestEntityManager testEntityManager;
+    private TestEntityManager testEntityManager; // Para manejar entidades en las pruebas
 
     @Test
     @DisplayName("Deberia retornar null cuando el medico se encuentre en consuta con otro paciente")
     void selectMedicalSpecialtyOnDateScenery1() {
-        //dado
+        // Dado: un médico y paciente registrados con una consulta el próximo lunes a las 10:00
         var nextMonday10H = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY))
                 .atTime(10,0);
 
-        var medic=registerMedic("julian","julian@mail.com","1234",Specialty.CARDIOLOGY);;
-        var patient=registerPatient("maria","maria@gmail.com","1234");
-        registerConsultation(medic,patient,nextMonday10H);
+        var medic = registerMedic("julian", "julian@mail.com", "1234", Specialty.CARDIOLOGY);
+        var patient = registerPatient("maria", "maria@gmail.com", "1234");
+        registerConsultation(medic, patient, nextMonday10H);
 
-        //caundo
-        var FreeMedic = medicalRepository.selectMedicalSpecialtyOnDate(Specialty.CARDIOLOGY,nextMonday10H );
+        // Cuando: se consulta un médico disponible de cardiología en esa fecha y hora
+        var FreeMedic = medicalRepository.selectMedicalSpecialtyOnDate(Specialty.CARDIOLOGY, nextMonday10H);
 
-        //entonces
+        // Entonces: debería retornar null ya que el médico está ocupado
         assertThat(FreeMedic).isNull();
     }
 
     @Test
-    @DisplayName("deberia retornar un medico cuando realice la consulta en la base de datos  en ese horario")
+    @DisplayName("Deberia retornar un medico cuando realice la consulta en la base de datos en ese horario")
     void selectMedicalSpecialtyOnDateScenery2() {
-
-        //dado
-        var nextMonday10H = LocalDate.now()
-                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+        // Dado: un médico registrado sin consultas el próximo lunes a las 10:00
+        var nextMonday10H = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.MONDAY))
                 .atTime(10,0);
 
-        var medic=registerMedic("julian","julian@mail.com","1234",Specialty.CARDIOLOGY);
+        var medic = registerMedic("julian", "julian@mail.com", "1234", Specialty.CARDIOLOGY);
 
-        //caundo
-        var FreeMedic = medicalRepository.selectMedicalSpecialtyOnDate(Specialty.CARDIOLOGY,nextMonday10H);
+        // Cuando: se consulta un médico disponible de cardiología en esa fecha y hora
+        var FreeMedic = medicalRepository.selectMedicalSpecialtyOnDate(Specialty.CARDIOLOGY, nextMonday10H);
 
-        //entonces
+        // Entonces: debería retornar el médico ya que está disponible
         assertThat(FreeMedic).isEqualTo(medic);
     }
 
+    // Método para registrar una consulta en la base de datos
     private void registerConsultation(Medic medic, Patient patient, LocalDateTime fecha) {
         testEntityManager.persist(new Query(null, medic, patient, fecha, null));
     }
 
+    // Método para registrar un médico en la base de datos
     private Medic registerMedic(String name, String email, String document, Specialty specialty) {
         var medic = new Medic(dataMedic(name, email, document, specialty));
         testEntityManager.persist(medic);
         return medic;
     }
 
+    // Método para registrar un paciente en la base de datos
     private Patient registerPatient(String name, String email, String document) {
         var patient = new Patient(dataPatient(name, email, document));
         testEntityManager.persist(patient);
         return patient;
     }
 
-    private  MedicalRegistrationData dataMedic(String name, String email, String document, Specialty specialty) {
+    // Datos para registrar un médico
+    private MedicalRegistrationData dataMedic(String name, String email, String document, Specialty specialty) {
         return new MedicalRegistrationData(
                 name,
                 email,
@@ -93,6 +95,7 @@ class MedicalRepositoryTest {
         );
     }
 
+    // Datos para registrar un paciente
     private PatientRegistrationData dataPatient(String name, String email, String document) {
         return new PatientRegistrationData(
                 name,
@@ -100,19 +103,17 @@ class MedicalRepositoryTest {
                 "61999999999",
                 document,
                 addressData()
-
         );
     }
 
+    // Datos de dirección
     private AddressData addressData() {
         return new AddressData(
-                " loca",
+                "loca",
                 "azul",
                 "acapulpo",
                 "321",
                 "12"
         );
     }
-
 }
-
